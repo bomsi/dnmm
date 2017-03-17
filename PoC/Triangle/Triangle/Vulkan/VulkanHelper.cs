fef43b279385f0df4ceb89b51a8ce6c19a258237
bomsi <bomsi@users.noreply.github.com>
2017-03-17 19:01:23 +0100

    Remove old non-.NET Standard code

--- PoC/Triangle/Triangle/Vulkan/VulkanHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Triangle.Vulkan
-{
-	internal static class VulkanHelper
-	{
-		internal static uint VK_API_VERSION_1_0 = VK_MAKE_VERSION(1, 0, 0);
-
-		internal static uint VK_MAKE_VERSION(uint major, uint minor, uint patch) => (major << 22) | (minor << 12) | patch;
-	}
-}
